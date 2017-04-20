d085a81b49369ec7fbd410272757d9ae7396e076
Renaud Schleck <renaud.schleck@free.fr>
2017-04-21 00:15:01 +0200

    Ajout de l'épreuve "trouver une paire"

--- infotsi/settings.py
+++ infotsi/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '((p7!ra7ng6b)5)jf3cuz^2wa@8c8@dr_g5!#8$-#vp(32zy%!'
 """
 Two things are wrong with Django's default `SECRET_KEY` system:
 
@@ -54,7 +53,7 @@ except NameError:
             to generate your secret key!' % SECRET_FILE)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['tsitroyes.pythonanywhere.com','localhost']
 
